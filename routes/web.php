6e026d6e0a4ca4e8b2bf91862acb341445a7cf8d
sidrastogi97 <siddharth.rastogi3@wipro.com>
2020-12-04 10:53:00 +0530

    Refactoring code

--- routes/web.php
+++ routes/web.php
@@ -28,16 +28,15 @@ Route::post('ApiSettings/Edit', 'App\Http\Controllers\view_api_controller@edit')
 Route::post('AddApi', 'App\Http\Controllers\add_api_controller@index');
 Route::get('OperatorList', 'App\Http\Controllers\TblMyOperatorController@show')->name('OperatorList');
 Route::get('OperatorList/Update/{api_id}/{operator_id}/{operator_api_id}', 'App\Http\Controllers\TblMyOperatorController@update');
+Route::get('OperatorStatus/Update/Status/{status_id}/{operator_id}', 'App\Http\Controllers\TblMyOperatorController@updateStatus');
+Route::get('OperatorAPI/Update/{operator_id}/{api_id}', 'App\Http\Controllers\TblMyOperatorController@removeAPI');
 Route::get('AmountFilter', 'App\Http\Controllers\TblAmountFilterController@show')->name('AmountFilter');
 Route::get('AmountFilter/{id}', 'App\Http\Controllers\TblAmountFilterController@show');
 Route::get('News', 'App\Http\Controllers\news@show')->name('News');
-<<<<<<< HEAD
 Route::get('ApiTrail', 'App\Http\Controllers\FailSwitchDetailController@index')->name('ApiTrail');
 Route::post('MapApi', 'App\Http\Controllers\FailSwitchDetailController@store');
-=======
 Route::post('AddNews', 'App\Http\Controllers\news@News_Data');
 Route::post('NewsDelete', 'App\Http\Controllers\news@destroy')->name('News.delete');
->>>>>>> cf2fa4dc7b291f030bfcdc4bb3a3bca4d1cd7cc1
 Route::post('AmountFilter/Update', 'App\Http\Controllers\TblAmountFilterController@update')->name('AmountFilter.update');
 Route::get('SmsSettings', 'App\Http\Controllers\sms_api_controller@index')->name('SmsSettings');
 Route::post('AddApiSms', 'App\Http\Controllers\add_sms_api@index');
