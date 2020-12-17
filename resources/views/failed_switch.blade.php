31647c754b9803dfc19f5f0d8abc7dfb4e9a25c6
sidrastogi97 <siddharth.rastogi3@wipro.com>
2020-12-18 00:32:39 +0530

    Fail Switch Issue fixed

--- resources/views/failed_switch.blade.php
+++ resources/views/failed_switch.blade.php
@@ -1,55 +1,60 @@
 @include('header')
-<link rel="stylesheet" href="/css/operator_list.css">
+<link rel="stylesheet" href="/css/account_list.css">
 
 <body>
+  <div class="container-fluid">
+    <div class = 'row mt-4 ml-3'>
+      <a href="/APITrailSettings">< Back to page</a>
+    </div>
+    <div class="row mt-4">
+      <div class="col-md-3 col-sm-9" >
 
-  <div class="row mt-5">
-    <div class="col-md-9 col-sm-9" style="padding: 0px;">
+        <div class="form-group" style="background-Position: 97% center;background-Repeat: no-repeat; cursor: pointer;" placeholder="SR1">
+          <select class="form-control" id="select">
+            <option value="0">--Select Operator--</option>
+            @for($i=0;$i<count($operator_data);$i++) @php if($operator_data[$i]->OperatorId == null)
+              {
+                echo '<option value='.$operator_data[$i]->id.'>'.$operator_data[$i]->operator.'</option>';
+              }
+              @endphp
 
-      <div class="form-group" style="background-Position: 97% center;background-Repeat: no-repeat; cursor: pointer;" placeholder="SR1">
-        <select class="form-control" id="select">
-
-          @for($i=0;$i<count($operator_data);$i++) @php if($operator_data[$i]->OperatorId == null)
-            {
-              echo '<option value='.$operator_data[$i]->id.'>'.$operator_data[$i]->operator.'</option>';
-            }
-            @endphp
-
-            @endfor
-        </select>
+              @endfor
+          </select>
+        </div>
       </div>
     </div>
-  </div>
-  @php
-  if(count($operator_data) !=0 )
-  {
-  @endphp
-  <div class=" table-responsive">
-    <table class="table table-striped table-bordered">
-      <tr class="bg-primary">
-        <th style="color:white !important">#</th>
-        <th style="color:white !important">API</th>
-        <th style="color:white !important">Minutes</th>
-        <th style="color:white !important">Priority</th>
-      </tr>
-
-      <tbody id="myTable">
-
-        @for($i=0;$i<count($api_master);$i++) <tr>
-          <th><input type="checkbox" name="checkbox" value="{{$api_master[$i]['id'] }}"> </th>
-          <th id="api{{$api_master[$i]->id}}"> {{$api_master[$i]->api_name }}</th>
-          <th><input type="text" id="minutes{{$api_master[$i]['id'] }}"></th>
-          <th><input type="text" id="priority{{$api_master[$i]['id'] }}" }}></th>
+    @php
+    if(count($operator_data) !=0 )
+    {
+    @endphp
+    <div>
+      <table id="faildedSwitch_table" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>API</th>
+            <th>Minutes</th>
+            <th>Priority</th>
           </tr>
-          @endfor
+        </thead>
 
-      </tbody>
-    </table>
-    <button onclick="AddapiClicked()" class="btn btn-primary">Add API </button>
+        <tbody id="myTable">
+          @for($i=0;$i<count($api_master);$i++) 
+            <tr>
+              <td><input type="checkbox" name="checkbox" value="{{$api_master[$i]['id'] }}"> </td>
+              <td id="api{{$api_master[$i]->id}}"> {{$api_master[$i]->api_name }}</td>
+              <td><input type="text" id="minutes{{$api_master[$i]['id'] }}"></td>
+              <td><input type="text" id="priority{{$api_master[$i]['id'] }}" }}></td>
+            </tr>
+            @endfor
+        </tbody>
+      </table>
+      <button onclick="AddapiClicked()" class="btn btn-primary">Add API </button>
+    </div>
+    @php
+    }
+    @endphp
   </div>
-  @php
-  }
-  @endphp
 
   <script>
     var master_id;
